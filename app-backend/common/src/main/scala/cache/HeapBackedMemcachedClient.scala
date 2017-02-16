3f7a9da6bc15f2849de424c662be7badce7e3bb7
Nathan Zimmerman <npzimmerman@gmail.com>
2017-02-16 14:42:16 -0500

    Move key sanitization to companion object

--- app-backend/common/src/main/scala/cache/HeapBackedMemcachedClient.scala
+++ app-backend/common/src/main/scala/cache/HeapBackedMemcachedClient.scala
@@ -12,19 +12,7 @@ class HeapBackedMemcachedClient[CachedType](
   client: MemcachedClient,
   options: HeapBackedMemcachedClient.Options = HeapBackedMemcachedClient.Options()) {
 
-  /** This key sanitizer replaces whitespace with '_' and throws in case of control characters */
-  def sanitizeKey(key: String): String = {
-    // Control characters in the unicode spec
-    val blacklist = "[^\u0020-\u007e^\u0009-\u000B]".r
-    assert(key.length <= 250, s"Keys of length 250 or greater are not allowed; key provided has length of ${key.length}")
-    assert(blacklist.findFirstIn(key) match {
-      case Some(char) => false
-      case None => true
-    } , s"Invalid use of control character ( ${blacklist.findFirstIn(key).get} ) detected in key")
-    val spaces = "[ \n\t\r]".r
-    spaces.replaceAllIn(key, "_")
-  }
-
+  /** The caffeine cache (on heap) which prevents race conditions */
   val onHeapCache: ScaffeineCache[String, Future[CachedType]] =
     Scaffeine()
       .recordStats()
@@ -33,7 +21,7 @@ class HeapBackedMemcachedClient[CachedType](
       .build[String, Future[CachedType]]()
 
   def caching(cacheKey: String)(expensiveOperation: String => Future[CachedType])(implicit ec: ExecutionContext): Future[CachedType] = {
-    val sanitizedKey = sanitizeKey(cacheKey)
+    val sanitizedKey = HeapBackedMemcachedClient.sanitizeKey(cacheKey)
     val futureCached: Future[CachedType] =
       onHeapCache.get(sanitizedKey, { cKey: String => client.getOrSet[CachedType](cKey, expensiveOperation, options.ttl) })
     onHeapCache.put(sanitizedKey, futureCached)
@@ -46,5 +34,18 @@ object HeapBackedMemcachedClient {
 
   def apply[CachedType](client: MemcachedClient, options: Options = Options()) =
     new HeapBackedMemcachedClient[CachedType](client, options)
+
+  /** This key sanitizer replaces whitespace with '_' and throws in case of control characters */
+  def sanitizeKey(key: String): String = {
+    val blacklist = "[^\u0020-\u007e]".r
+    assert(key.length <= 250, s"Keys of length 250 or greater are not allowed; key provided has length of ${key.length}")
+    assert(blacklist.findFirstIn(key) match {
+      case Some(char) => false
+      case None => true
+    } , s"Invalid use of control character ( ${blacklist.findFirstIn(key).get} ) detected in key")
+    val spaces = "[ \n\t\r]".r
+    spaces.replaceAllIn(key, "_")
+  }
+
 }
 
--- app-backend/common/src/test/scala/cache/HeapBackedMemcachedClientSpec.scala
+++ app-backend/common/src/test/scala/cache/HeapBackedMemcachedClientSpec.scala
@@ -7,14 +7,12 @@ import java.net._
 
 
 class HeapBackedMemcachedClientSpec extends FunSpec with Matchers {
-  val cli: MemcachedClient = new MemcachedClient(new InetSocketAddress("0.0.0.0", 1111))
-  val hbmc = HeapBackedMemcachedClient[Int](cli)
 
   it("should throw if passed a string that is too long") {
     // Keys need to be <250 characters long
     val keyTooLong: String = (1 to 250).map(_.toString).foldLeft("")(_ ++ _)
     an [AssertionError] should be thrownBy {
-      hbmc.sanitizeKey(keyTooLong)
+      HeapBackedMemcachedClient.sanitizeKey(keyTooLong)
     }
   }
 
@@ -22,7 +20,7 @@ class HeapBackedMemcachedClientSpec extends FunSpec with Matchers {
     // The Memcached key spec prohibits control characters
     val keyInvalid: String = " ⌫ "
     an [AssertionError] should be thrownBy {
-      hbmc.sanitizeKey(keyInvalid)
+      HeapBackedMemcachedClient.sanitizeKey(keyInvalid)
     }
   }
 
@@ -30,14 +28,14 @@ class HeapBackedMemcachedClientSpec extends FunSpec with Matchers {
     // The Memcached key spec prohibits control characters
     val keyValid: String = "test!"
     noException should be thrownBy {
-      hbmc.sanitizeKey(keyValid)
+      HeapBackedMemcachedClient.sanitizeKey(keyValid)
     }
   }
 
   it("should replace whitespace with underscores") {
     // The Memcached key spec prohibits control characters
-    val keyWhiteSpace: String = " x\tx\nx\r"
-    hbmc.sanitizeKey(keyWhiteSpace) shouldEqual ("_x_x_x_")
+    val keyWhiteSpace: String = "This is a test."
+    HeapBackedMemcachedClient.sanitizeKey(keyWhiteSpace) shouldEqual ("This_is_a_test.")
   }
 }
 
